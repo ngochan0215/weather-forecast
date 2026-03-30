5c6240c59e1e9aa770a7498844075d6793213c65
phanthingochan <ngochan2005blislife@gmail.com>
2026-03-30 22:46:53 +0700

    Weather Object

---
+++ Ex3_WeatherObject.ts
@@ -0,0 +1,7 @@
+const currentWeather = {
+    temperature: 85,
+    conditions: "super sunny",
+};
+
+console.log("Current weather:", currentWeather);
+console.log(`It's ${currentWeather.temperature} degrees F and ${currentWeather.conditions}.`);
