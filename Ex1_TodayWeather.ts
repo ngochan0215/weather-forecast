c15db0767ed9db636fb745f93e5561496a0670b2
phanthingochan <ngochan2005blislife@gmail.com>
2026-03-30 16:20:45 +0700

    Today's weather

---
+++ Ex1_TodayWeather.ts
@@ -0,0 +1,7 @@
+const temperature = 25;
+const conditions = "sunny";
+const sunriseTime = "5:45 AM";
+const sunsetTime = "17:45 PM";
+
+console.log(`It's currently ${conditions} and ${temperature} degrees. 
+    The sun rose this morning at ${sunriseTime} and will set at ${sunsetTime}.`);
